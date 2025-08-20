f7378787f0a7c00fbee4dd12a866c95f608a80fa
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-08-20 23:03:29 +0800

    Add greetings

---
+++ src/main/java/Dino.java
@@ -0,0 +1,26 @@
+public class Dino {
+    public static void main(String[] args) {
+        /*String logo = " ____   _           \n"
+
+                + "|  _ \\ |_| _____   ___ \n"
+                + "| | | || ||  _  \\ /   \\\n"
+                + "| |_| || || | | ||     |\n"
+                + "|____/ |_||_| |_| \\___/\n";
+
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        System.out.println("Hello from\n" + logo);*/
+
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Dino. \nWhat can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(line);
+    }
+}

0abad72ddd473fa0398301473a8203099bf5e7b9
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-08-21 00:27:57 +0800

    Store the text and ready for display

--- src/main/java/Dino.java
+++ src/main/java/Dino.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class Dino {
+    static final String line = "____________________________________________________________";
+    static final int MAX_NUM = 100;
+    static int curr = 0;
+    static String[] tasks = new String[MAX_NUM];
+
+    private static void printMessage(String message) {
+        System.out.println(line);
+        System.out.println("added: " + message);
+        System.out.println(line);
+    }
+
+    private static void printList(String[] tasks) {
+        System.out.println(line);
+        for (int i = 0; i < curr; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
-        /*String logo = " ____   _           \n"
-
-                + "|  _ \\ |_| _____   ___ \n"
-                + "| | | || ||  _  \\ /   \\\n"
-                + "| |_| || || | | ||     |\n"
-                + "|____/ |_||_| |_| \\___/\n";
-
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        System.out.println("Hello from\n" + logo);*/
 
         Scanner scanner = new Scanner(System.in);
 
-        String line = "____________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Dino. \nWhat can I do for you?");
         System.out.println(line);
@@ -32,12 +36,13 @@ public class Dino {
                 System.out.println("Bye. Hope to see you soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                printList(tasks);
+            } else {
+                tasks[curr] = input;
+                curr += 1;
+                printMessage(input);
             }
-
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
-
         }
     }
 }

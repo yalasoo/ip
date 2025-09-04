8ded1a76abffd615ad682272f3ba15ee8881cf31
yalasoo <194611248+yalasoo@users.noreply.github.com>
2025-09-04 16:54:11 +0800

    Add Parser class

--- src/main/java/Dino.java
+++ src/main/java/Dino.java
@@ -16,96 +16,71 @@ public class Dino {
         storage.saveData(tasks);
     }
 
-    public void run() {
+    public void run() throws DukeException {
         ui.showWelcome();
 
         while (true) {
             String input = ui.readCommand();
+            String[] parsedCommand = Parser.parse(input);
+            int len = parsedCommand.length;
+            String commandType = parsedCommand[0];
+            String commandDetail = len > 1 ? parsedCommand[1] : null;
+            String extraDetail = len > 2 ? parsedCommand[2] : null;
+            String finalExtraDetail = len > 3 ? parsedCommand[3] : null;
 
             try {
-                if (input.equals("bye")) {
-                    ui.showBye();
-                    break;
-                } else if (input.equals("list")) {
-                    ui.showTaskList(tasks);
-                } else if (input.startsWith("mark")) {
-                    String index = input.substring(5).trim();
-                    int i = Integer.parseInt(index) - 1;
-                    if (i < 0 || i >= tasks.size()) {
-                        throw new DukeException("The task does not exist.");
+                switch (commandType) {
+                    case "bye":
+                        ui.showBye();
+                        return;
+
+                    case "list":
+                        ui.showTaskList(tasks);
+                        break;
+
+                    case "mark": {
+                        int index = Integer.parseInt(commandDetail) - 1;
+                        tasks.get(index).markAsDone();
+                        ui.showTaskMarked(tasks.get(index));
+                        break;
                     }
-                    tasks.get(i).markAsDone();
-                    ui.showTaskMarked(tasks.get(i));
-                } else if (input.startsWith("unmark")) {
-                    String index = input.substring(7).trim();
-                    int i = Integer.parseInt(index) - 1;
-                    if (i < 0 || i >= tasks.size()) {
-                        throw new DukeException("The task does not exist.");
+
+                    case "unmark": {
+                        int index = Integer.parseInt(commandDetail) - 1;
+                        tasks.get(index).markAsUndone();
+                        ui.showTaskUnmarked(tasks.get(index));
+                        break;
                     }
-                    tasks.get(i).markAsUndone();
-                    ui.showTaskUnmarked(tasks.get(i));
-                } else if (input.startsWith("todo")) {
-                    String description = input.substring(5).trim();
-                    if (description.isEmpty()) {
-                        throw new DukeException("The description of a todo cannot be empty.");
+
+                    case "todo": {
+                        Task todo = new Todo(commandDetail);
+                        tasks.add(todo);
+                        ui.showTaskAdded(todo, tasks);
+                        break;
                     }
-                    Task todo = new Todo(description);
-                    tasks.add(todo);
-                    ui.showTaskAdded(todo, tasks);
-                } else if (input.startsWith("deadline")) {
-                    String[] detail = input.substring(9).split("/by");
-                    if (detail.length != 2) {
-                        throw new DukeException("Use the correct format!");
+
+                    case "deadline": {
+                        Task deadline = new Deadline(commandDetail, extraDetail);
+                        tasks.add(deadline);
+                        ui.showTaskAdded(deadline, tasks);
+                        break;
                     }
-                    String description = detail[0].trim();
-                    if (description.isEmpty()) {
-                        throw new DukeException("The description of a deadline cannot be empty.");
+
+                    case "event": {
+                        Task event = new Event(commandDetail, extraDetail, finalExtraDetail);
+                        tasks.add(event);
+                        ui.showTaskAdded(event, tasks);
+                        break;
                     }
-                    String date = detail[1].trim();
-                    if (date.isEmpty()) {
-                        throw new DukeException("Deadline needs a date!");
+
+                    case "delete": {
+                        int index = Integer.parseInt(commandDetail) - 1;
+                        Task deleted = tasks.remove(index);
+                        ui.showTaskDeleted(deleted, tasks);
+                        break;
                     }
-                    Task deadline = new Deadline(description, date);
-                    tasks.add(deadline);
-                    ui.showTaskAdded(deadline, tasks);
-                } else if (input.startsWith("event")) {
-                    String[] detail = input.substring(6).split("/from");
-                    if (detail.length != 2) {
-                        throw new DukeException("Missing description or time!");
-                    }
-                    String description = detail[0].trim();
-                    if (description.isEmpty()) {
-                        throw new DukeException("The description of an event cannot be empty.");
-                    }
-                    String[] duration = detail[1].split("/to");
-                    if (duration.length != 2) {
-                        throw new DukeException("Missing start or end time!");
-                    }
-                    String start = duration[0].trim();
-                    if (start.isEmpty()) {
-                        throw new DukeException("Event needs a start time!");
-                    }
-                    String end = duration[1].trim();
-                    if (end.isEmpty()) {
-                        throw new DukeException("Event needs an end time!");
-                    }
-                    Task event = new Event(description, start, end);
-                    tasks.add(event);
-                    ui.showTaskAdded(event, tasks);
-                } else if (input.startsWith("delete")) {
-                    String index = input.substring(7).trim();
-                    int i = Integer.parseInt(index) - 1;
-                    if (i < 0 || i >= tasks.size()) {
-                        throw new DukeException("Choose a valid task.");
-                    }
-                    Task deleted = tasks.remove(i);
-                    ui.showTaskDeleted(deleted, tasks);
-                } else {
-                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
                 saveTasks();
-            } catch (DukeException e) {
-                ui.showError("Check again! " + e.getMessage());
             } catch (IOException e) {
                 ui.showError("Failed to save data:" + e.getMessage());
             }
@@ -115,6 +90,9 @@ public class Dino {
     public static void main(String[] args) {
         try {
             new Dino("data/tasks.txt").run();
+        } catch (DukeException e) {
+            Ui ui = new Ui();
+            ui.showError("Check again!" + e.getMessage());
         } catch (IOException e) {
             Ui ui = new Ui();
             ui.showError("Failed to load tasks: " + e.getMessage());
---
+++ src/main/java/Parser.java
@@ -0,0 +1,69 @@
+public class Parser {
+    public static String[] parse(String command) throws DukeException {
+        String[] parts = command.trim().split(" ", 2);
+        String commandType = parts[0];
+        String commandDetail = parts.length > 1 ? parts[1] : "";
+
+        switch (commandType) {
+            case "bye":
+            case "list":
+            case "mark": {
+                if (commandDetail.isEmpty()) {
+                    throw new DukeException("Choose a valid task number to mark a task.");
+                }
+                return new String[] {commandType, commandDetail};
+            }
+            case "unmark": {
+                if (commandDetail.isEmpty()) {
+                    throw new DukeException("Choose a valid task number to unmark a task.");
+                }
+                return new String[] {commandType, commandDetail};
+            }
+            case "todo": {
+                if (commandDetail.isEmpty()) {
+                    throw new DukeException("The description of a todo cannot be empty.");
+                }
+                return new String[]{commandType, commandDetail};
+            }
+            case "deadline": {
+                String[] taskDetail = commandDetail.split("/by");
+                String description = taskDetail[0].trim();
+                if (description.isEmpty()) {
+                    throw new DukeException("The description of a deadline cannot be empty.");
+                }
+                String date = taskDetail[1].trim();
+                if (date.isEmpty()) {
+                    throw new DukeException("Deadline needs a date!");
+                }
+                return new String[]{commandType, description, date};
+            }
+            case "event": {
+                String[] taskDetail = commandDetail.split("/from");
+                if (taskDetail.length != 2) {
+                    throw new DukeException("Missing description or time!");
+                }
+                String description = taskDetail[0].trim();
+                if (description.isEmpty()) {
+                    throw new DukeException("The description of an event cannot be empty.");
+                }
+                String[] duration = taskDetail[1].split("/to");
+                if (duration.length != 2) {
+                    throw new DukeException("Missing start or end time!");
+                }
+                String start = duration[0].trim();
+                if (start.isEmpty()) {
+                    throw new DukeException("Event needs a start time!");
+                }
+                String end = duration[1].trim();
+                if (end.isEmpty()) {
+                    throw new DukeException("Event needs an end time!");
+                }
+                return new String[]{commandType, description, start, end};
+            }
+            case "delete": {
+                return new String[]{commandType, commandDetail};
+            }
+            default: throw new DukeException("I'm sorry, but I don't know what that means :-(");
+        }
+    }
+}
